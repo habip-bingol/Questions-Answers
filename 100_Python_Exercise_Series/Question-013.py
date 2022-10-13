f648b6667aadb57d80857c9bd16be7c37c577bb3
Habip <hbpbngl25@gmail.com>
2022-10-13 15:26:16 +0300

    Q-13 was added

---
+++ 100_Python_Exercise_Series/Question-013.py
@@ -0,0 +1,24 @@
+# Question:
+
+# Write a program that accepts a sentence and calculate the number of letters and digits.
+# Suppose the following input is supplied to the program:
+
+# hello world! 123
+
+# Then, the output should be:
+
+# LETTERS 10
+# DIGITS 3
+
+sentence = input("Please enter a sentence : ")
+letters = []
+digits = []
+for i in sentence:
+    if i.isdigit():
+        digits.append(i)
+    elif i.isalpha():
+        letters.append(i)
+    else:
+        pass
+
+print(f"In this sentence There is {len(letters)} letters and {len(digits)} digits.")
