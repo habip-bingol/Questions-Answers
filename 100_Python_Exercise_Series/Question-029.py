1f2cd20aeaa6820a17bafaae6178cc6f8d95e113
Habip Bingöl <hbpbngl25@gmail.com>
2022-11-28 15:39:32 +0300

    Q29 added

---
+++ 100_Python_Exercise_Series/Question-029.py
@@ -0,0 +1,8 @@
+# Define a function that can receive two integral numbers in string form and compute their sum and then print it in console.
+
+def sumStr(n1, n2):
+    n1 = int(n1)
+    n2 = int(n2)
+    return n1 + n2
+
+sumStr("45","213")
