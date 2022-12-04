813219621c916f70b4d956a1be61db9965e2649e
Habip Bingöl <hbpbngl25@gmail.com>
2022-12-05 00:21:20 +0300

    Q32 added

---
+++ 100_Python_Exercise_Series/Question-032.py
@@ -0,0 +1,13 @@
+# Define a function that can accept an integer number as input and print the "It is an even number" 
+# if the number is even, otherwise print "It is an odd number".
+
+def check_num(num):
+    if num%2 == 0:
+        print("It is an even number")
+    else :
+        print("It is an odd number")
+        
+        
+# check_num(13)
+# check_num(14)
+
