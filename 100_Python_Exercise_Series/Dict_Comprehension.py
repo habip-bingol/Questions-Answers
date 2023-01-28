63c44a08d46f1de688b4ddb067ba5a8ff8bea346
Habip Bingöl <hbpbngl25@gmail.com>
2023-01-28 09:58:55 +0300

    dict_comp que was added

---
+++ 100_Python_Exercise_Series/Dict_Comprehension.py
@@ -0,0 +1,13 @@
+# Create a dictionary with Dict Comprehensions from the lists below which is appropriate, such as: {"TR": "Turkey"}.
+    
+# country_code_list = ["TR", "US", "CN", "SE", "FR", "NL"]
+# country_list = ["Netherlands", "France", "Sweden", "United States", "China", "Turkey"]
+
+
+
+country_code_list = ["TR", "US", "CN", "SE", "FR", "NL"]
+country_list = ["Netherlands", "France", "Sweden", "United States", "China", "Turkey"]
+
+country_dict = {i:j for i in country_code_list for j in country_list if i[0] == j[0]}
+
+print(country_dict)
