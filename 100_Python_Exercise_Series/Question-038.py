e1b15c972e7d77f3b55610ceb6fca2f128b4f38b
Habip Bingöl <hbpbngl25@gmail.com>
2023-02-21 10:06:00 +0300

    Q38 was added

---
+++ 100_Python_Exercise_Series/Question-038.py
@@ -0,0 +1,10 @@
+# Define a function which can generate a list where the values are square of numbers between 1 and 20 (both included). Then the function needs to print the first 5 elements in the list.
+
+def square_list():
+    squares = []
+    for i in range(1,21):
+        squares.append(i**2)
+    print(squares[:5])
+        
+    
+square_list()            
